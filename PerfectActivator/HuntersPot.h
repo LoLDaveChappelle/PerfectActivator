31c522beb0d24a5e667d51a27a19ae9b4f441402
LoLDaveChappelle <gskillpro@gmail.com>
2017-02-09 13:46:43 -0600

    Update HuntersPot.h
    Added usage of healthpot

--- PerfectActivator/HuntersPot.h
+++ PerfectActivator/HuntersPot.h
@@ -6,17 +6,27 @@
 class HuntersPot
 {
 public:
-	IMenuOption* UseHealthPot;
-	IMenuOption* HealthPercent;
+
 	IMenu* HealthPotionMenu;
+	IMenuOption* UseHealthPot;
+	IMenuOption* HunterHealthPercent;
 	int HealthPotionID = 2033;
+	IInventoryItem* pItem = GPluginSDK->CreateItemForId(2032, 100);
 
 	void  Menu()
 	{
 		HealthPotionMenu = ItemMenu->ConsumablesMenu->AddMenu("Hunters Potion");
 		UseHealthPot = HealthPotionMenu->CheckBox("Use Hunters Potion", true);
-		HealthPercent = HealthPotionMenu->AddInteger("Hero Health %", 0, 100, 50);
+		HunterHealthPercent = HealthPotionMenu->AddInteger("Hero Health %", 0, 100, 50);
 	}
 
+	void UseHuntersPotion()
+	{
+		if (GEntityList->Player()->HasBuff("RegenerationPotion"))
+		{
+			return;
+		}
+		pItem->CastOnPlayer();
+	}
 
 };
